f53467080b48c4b3a4d85e122802c22c50addffc
kapsali29 <kapsali29@gmail.com>
2018-09-04 23:26:48 +0300

    unigrams functionality added

--- process.py
+++ process.py
@@ -1,3 +1,6 @@
+from nltk.tokenize import RegexpTokenizer
+
+
 def read_files():
     """
     The following function is used to read the files from the Qualifications folder.
@@ -11,6 +14,22 @@ def read_files():
         list_of_texts.append(lines)
     return list_of_texts
 
-total_texts = read_files()
-print(total_texts[89])
 
+def tranform_to_text_to_unigrams(list_of_texts):
+    """
+    The following function transforms the qualification texts to unigrams.
+    :param list_of_texts: list of texts
+    :return: list of unigrams
+    """
+    tokenizer = RegexpTokenizer(r'\w+')
+    list_of_unigrams = []
+    for text in list_of_texts:
+        joined_text = ",".join(text)
+        text_unigrams = tokenizer.tokenize(joined_text)
+        list_of_unigrams.append(text_unigrams)
+    return list_of_unigrams
+
+
+list_of_texts = read_files()
+list_of_unigrams = tranform_to_text_to_unigrams(list_of_texts=read_files())
+print(list_of_unigrams[78])
